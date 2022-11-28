11a002e9bcce7528ca54dae297f8c48aeb79d566
Matheus-Pontes <pontesm10@outlook.com>
2022-11-28 19:13:21 -0300

    Retirando 'usings' que não estavam sendo utilizados.

--- Searchs/Services/SequencialSearch.cs
+++ Searchs/Services/SequencialSearch.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Searchs
+﻿namespace Searchs
 {
     public class SequencialSearch : SearchInterface
     {
@@ -17,6 +11,7 @@ namespace Searchs
                 if (numberSearch == array[i])
                     position = i;
             }
+
 
             return position;
         }
